2b15a27dc2d956e1be6d32435f16bb4fd4bb4645
Naufal <naufalalvaro0212@gmail.com>
2026-08-06 21:09:25 +0700

    feat: mengubah isi create.blade.php

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -24,13 +24,13 @@
             </select>
 
             <x-primary-button :disabled="false" x-bind:disabled="loading">
-                <span x-show="!loading">Simpan</span>
+                <span x-show="!loading">Menyimpan</span>
                 <span x-show="loading" class="flex items-center gap-2">
                     <svg class="animate-spin h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
                     </svg>
-                    Menyimpan...
+                    simpan
                 </span>
             </x-primary-button>
         </form>
